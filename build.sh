36e1e450df994fb089db8084539c3205186008da
Jordan S Read <jread@usgs.gov>
2014-10-02 15:15:24 -0500

    starting fresh

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./build_glm.sh
-
--- clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./clean_glm.sh
-
-# NB Some versions of sed may do horrid things to your source 
-#    if you use this script.
-./clean_blanks.sh
