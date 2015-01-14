68cdba7aad253ca3c563ed3259e053f54e7f7ac1
Luke Winslow <lwinslow@usgs.gov>
2015-01-14 09:22:03 -0600

    Second commit back to custom build script

--- build_glm.sh
+++ build_glm.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+
+## Some info:
+# If your netcdf library is not available on PATH, you need to define
+# its location using the environment variable NETCDFLIB. 
+# Example: export NETCDFLIB=/opt/local/lib
+#
+
+
+
 export FABM=true
+
 export AED2=true
 export USE_DL=false
 
@@ -8,36 +18,27 @@ export FORTRAN_COMPILER=IFORT
 # export FORTRAN_COMPILER=GFORTRAN
 # export FORTRAN_COMPILER=OPEN64
 
-export OSTYPE=`uname -s`
-
 if [ "$FORTRAN_COMPILER" = "IFORT" ] ; then
    . /opt/intel/bin/compilervars.sh intel64
    export PATH="/opt/intel/bin:$PATH"
    export FC=ifort
-   export NETCDFHOME=/opt/intel
 elif [ "$FORTRAN_COMPILER" = "IFORT11" ] ; then
    . /opt/intel/Compiler/11.1/072/bin/ifortvars.sh intel64
    export PATH="/opt/intel/bin:$PATH"
    export FC=ifort
-   #export NETCDFHOME=/opt/intel
-   export NETCDFHOME=/usr
 elif [ "$FORTRAN_COMPILER" = "IFORT12" ] ; then
    . /opt/intel/bin/compilervars.sh intel64
    export PATH="/opt/intel/bin:$PATH"
    export FC=ifort
-   export NETCDFHOME=/opt/intel
 elif [ "$FORTRAN_COMPILER" = "OPEN64" ] ; then
    . /opt/open64/open64_env.sh
    #export PATH="/opt/open64/bin:$PATH"
    export FC=openf95
-   export NETCDFHOME=/opt/open64
 else
    export FC=gfortran
-   export NETCDFHOME=/usr
-   if [ "$OSTYPE" == "Darwin" ] ; then
-     export NETCDFHOME=/opt/local
-   fi
 fi
+
+export OSTYPE=`uname -s`
 
 export F77=$FC
 export F90=$FC
@@ -45,20 +46,15 @@ export F95=$FC
 
 export MPI=OPENMPI
 
-export NETCDFINC=$NETCDFHOME/include
-export NETCDFINCL=${NETCDFINC}
-export NETCDFLIBDIR=$NETCDFHOME/lib
-export NETCDFLIB=${NETCDFLIBDIR}
-export NETCDFLIBNAME="-lnetcdff -lnetcdf"
+
 export CURDIR=`pwd`
-export FABMDIR=${CURDIR}/fabm-git
+if [ "${FABM_NEW_BUILD}" = "true" ] ; then
+  export FABMDIR=${CURDIR}/FABM-new/fabm-git
+else
+  export FABMDIR=${CURDIR}/fabm-git
+fi
 export PLOTDIR=${CURDIR}/libplot
 export AED2DIR=${CURDIR}/libaed2
-
-if [ ! -d $FABMDIR ] ; then
-   echo "FABM directory not found - building a non-FABM version"
-   export FABM=false
-fi
 
 export COMPILATION_MODE=production
 
@@ -79,9 +75,9 @@ if [ "${FABM}" = "true" ] ; then
   make || exit 1
 fi
 
-if [ "$OSTYPE" != "Darwin" ] ; then
+if [ "$OSTYPE" != "Darwin" ] && [ "$OSTYPE" != "Linux" ] ; then
   cd ${PLOTDIR}
-  make || exit 1
+ make || exit 1
 fi
 
 cd ${CURDIR}/libutil
@@ -95,7 +91,7 @@ make || exit 1
 
 cd ${CURDIR}/glm-aed
 
-if [ "$OSTYPE" == "Linux" ] ; then
+if [ "$OSTYPE" != "Darwin" ]  && [ "$OSTYPE" != "Linux" ] ; then
   VERSION=`grep GLM_VERSION src/glm.h | cut -f2 -d\"`
   echo glm version $VERSION
   VERSDEB=`head -1 debian/changelog | cut -f2 -d\( | cut -f1 -d-`
