dbaefc6a3703223cd1c77d2dc2cb634573b6c043
Luke Winslow <lwinslow@usgs.gov>
2015-05-01 07:25:42 -0500

    Add snow to volume budget when no ice present

--- glm-aed/src/glm_surface.c
+++ glm-aed/src/glm_surface.c
@@ -628,7 +628,11 @@ void do_surface_thermodynamics(int jday, int iclock, int LWModel,
         // depths to get new composition. firstly evaporation
         Lake[surfLayer].Height += (SurfData.Evap * noSecs + (MetData.Rain) * (noSecs / SecsPerDay));
 	if ( catchrain ) Lake[surfLayer].Height += catch_runoff / Lake[surfLayer].LayerArea;
-
+        
+        // Add snow directly to surface layer height if there is no ice. 
+        // If there is ice, snow will be handled in the next block
+        Lake[surfLayer].Height += MetData.Snow * Lake[surfLayer].LayerArea * (RHOSNO/rho0) * (noSecs / SecsPerDay)
+        
         recalc_surface_salt();
     }
 
